2d977f323b0dd892ea922e0763e240de97dc94fe
thielen4 <jenny.thielen@kellogg.northwestern.edu>
2020-02-18 02:23:20 +0000

    class 7 updates

--- dice.rb
+++ dice.rb
@@ -4,5 +4,7 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-  "Hello"
+    @die_1 = rand(1..6)
+    @die_2 = rand(1..6)
+        view "dice"
 end
